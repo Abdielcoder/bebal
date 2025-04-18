37a7ad7c8cb34d762d3665b5eaece5a0767d098b
Abdiel C <redskullcoder@gmail.com>
2025-04-18 12:25:23 -0700

    Boton imprimir-5

--- ajax/buscar_principal_nuevo.php
+++ ajax/buscar_principal_nuevo.php
@@ -436,7 +436,7 @@ echo '<font size="1" color="black">Tramite:</font> <font size="1" color="blue">'
                                 // Botón de "Presupuesto"
                                 if ($estatus == "Presupuesto") {
                                     $mostrarBotonAmarillo = true;
-                                    $botonAmarilloHTML .= '<button type="button" class="btn amarillo-bottom" style="background-color:#ffc107 !important; color:white !important;" title="Presupuesto" onclick="presupuesto(\''.$id.'\')">Presupuesto</button>';
+                                    $botonAmarilloHTML .= '<button type="button" class="btn amarillo-bottom" style="background-color:#ffc107 !important; color:white !important;" title="Presupuesto" onclick="presupuesto(\''.$id.'\')"><i class="bi bi-receipt"></i> Presupuesto</button>';
                                 }
                                 
                                 // Si hay algún botón amarillo para mostrar, añadir el contenedor
